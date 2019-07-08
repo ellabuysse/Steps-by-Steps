542f26cd6ab7dd55f0f06b2882fc8e41c2b7093d
Zhang <czhang.ocean@gmail.com>
2019-07-08 14:50:39 -0700

    implementing changes on other pages

---
+++ src/WIMPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import NavBar from './NavBar/NavBar';
+import FeatureBox from './FeatureBox/FeatureBox';
+import tips from './Data/tips';
+
+const WIMPage = (props) => {
+    return (
+        <div>
+            <div className="tips-container">
+                {
+                    tips.map(value => (
+                        <FeatureBox
+                            smallGraphic={value.icon}
+                            tipText={value.tipText}
+                            key={value.tipText}
+                        />
+                    ))
+                } 
+                </div>
+                <div className="paragraph">
+                    <p>Message here about why you should drive</p>
+                </div>
+        </div>
+    
+    );
+};
+
+export default WIMPage
