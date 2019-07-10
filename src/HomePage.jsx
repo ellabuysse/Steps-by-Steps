4113cd327fa3259b9766f5ef63508ac471af6c93
Ella Buysse <buyssee@sea-buyssee-m.ds.ad.adp.com>
2019-07-10 10:00:08 -0700

    Merged

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar';
+import './HomePage.css';
 
 const HomePage = (props) => {
     return (
         <div>
-            <NavBar/>
-            <h1>Steps by Steps</h1>
-            <h2>Saving the earth one step at a time</h2>
+            <div id="body">
+                <div id="words">
+                    <h1>STEPS BY STEPS</h1>
+                    <h2>Saving the earth one step at a time</h2>
+                </div>
+            </div>
         </div>
     );
 }
