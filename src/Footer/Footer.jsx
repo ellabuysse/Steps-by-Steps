9af1ddd156d6a733468135a5538d5c58d34c067f
Zhang <czhang.ocean@gmail.com>
2019-07-15 08:24:26 -0700

    transportation bg img in tips page shows bikes, not side of building

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './Footer.css';
-import {
-    Link
-} from 'react-router-dom';
 
 export default class Footer extends Component {
 
--- src/TipsPage/TipsPage.jsx
+++ src/TipsPage/TipsPage.jsx
@@ -16,11 +16,12 @@ const TipsPage = (props) => {
             <h1 className="page-title">What You Can Do</h1>
             <div className="bg-image">
                 {
+                <div className="transport">
                     <BGImage 
                         img="https://images.unsplash.com/photo-1500748851664-017ef7f22878?ixlib=rb-1.2.1&auto=format&fit=crop&w=2562&q=80"
                         title="Transportation"
-                        position="bottom"
                         />
+                </div>
                 }
             </div>
             <div className="tips-container top">
