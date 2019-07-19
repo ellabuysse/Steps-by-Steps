99f33bbc79dc9ec7816c8fb7fbc70c7c0cca684b
Zhang <czhang.ocean@gmail.com>
2019-07-19 08:48:19 -0700

    tried to clean up css

--- src/BGImage/BGImage.jsx
+++ src/BGImage/BGImage.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
-import './BGImage.css';
 
 export default class BGImage extends Component {
     
-   
-
     render() {
         return( 
             <div className="container" style={{
