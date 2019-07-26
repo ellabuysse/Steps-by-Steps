53c2a96f6fd3d7fd4d23e8acd6f933a1ccb6c945
Ella Buysse <buyssee@sea-buyssee-m.ds.ad.adp.com>
2019-07-26 15:28:21 -0700

    made quiz-bg-image shorter

--- src/QuizPage/QuizPage.jsx
+++ src/QuizPage/QuizPage.jsx
@@ -7,25 +7,27 @@ import './QuizPage.css';
 
 const QuizPage = (props) => {
     return (
-        <div>
+     <div>
             <NavBar/>
            
             <div className="bg-image">
-                {
-                    <BGImage 
-                        img="https://images.pexels.com/photos/1115090/pexels-photo-1115090.jpeg?cs=srgb&dl=beautiful-flowers-bloom-blossom-1115090.jpg&fm=jpg"
-                        title="Environmental Impact Quiz"
-                        position="bottom"
-                        />
-                }
+                
+                    <div className="quiz-photo-container" style={{
+                        
+                    }}>
+                            <div className="title"><strong>Environmental Impact Quiz</strong></div>
+                        
+                    </div>
+                    </div>
+                   
             <div className="page-container">
                 <p className="quiz-item" id="quiz-description">Take our quiz to determine the impact you have on the environment. Evaluate yourself on the previous week and complete weekly to see how your steps for a reduced impact are making a difference. </p>
                 <QuizForm/>
             </div>
-        </div>
+             
         <Footer/>
-        
-</div>
+        </div>
+
     );
 };
 export default QuizPage;
