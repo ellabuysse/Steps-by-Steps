fa574881c7a8e59b9d459a1dd207a9db87ca3840
Zhang <czhang.ocean@gmail.com>
2019-07-30 16:39:54 -0700

    got sidenav dots to get bigger and fixed formatting

--- src/ScrollNav/ScrollNavTips.jsx
+++ src/ScrollNav/ScrollNavTips.jsx
@@ -1,63 +1,60 @@
 import React, { Component } from 'react';
+import { Link, animateScroll as scroll } from "react-scroll";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'  
 import './ScrollNav.css';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-export default class ScrollNav extends Component {
-    constructor(props){
-        super(props)
-        this.state ={
-            firstButton: true,
-            secondButton: false,
-            thirdButton: false,
-            fourthButton: false
-        }
-        this.handleClickFirst = this.handleClickFirst.bind(this);
-        this.handleClickSecond = this.handleClickSecond.bind(this);
-        this.handleClickThird = this.handleClickThird.bind(this);
-        this.handleClickFourth = this.handleClickFourth.bind(this);
-    }
-    handleClickFirst(){
-        this.setState({
-            firstButton: true,
-            secondButton: false,
-            thirdButton: false,
-            fourthButton: false
-        })
-    
-    }
-    handleClickSecond(){
-        this.setState({
-            firstButton: false,
-            secondButton: true,
-            thirdButton: false,
-            fourthButton: false
-        })
-    }
-    handleClickThird(){
-        this.setState({
-            firstButton: false,
-            secondButton: false,
-            thirdButton: true,
-            fourthButton: false
-        })
-    }
-    handleClickFourth(){
-        this.setState({
-            firstButton: false,
-            secondButton: false,
-            thirdButton: false,
-            fourthButton: true
-        })
-    }
-    render() {
 
+export default class ScrollNav extends Component {
+    render() {
         return(
             <div className = "scroll-nav" id="tips-scroll">
-                <AnchorLink href='#transport' className={this.state.firstButton ? "buttonTrueTips": "buttonFalseTips"} onClick={this.handleClickFirst}><FontAwesomeIcon icon= {faCircle} /></AnchorLink>
-                <AnchorLink href='#food' className={this.state.secondButton ? "buttonTrueTips": "buttonFalseTips"} onClick={this.handleClickSecond}><FontAwesomeIcon icon= {faCircle} /></AnchorLink> 
-                <AnchorLink href='#buying' className={this.state.thirdButton? "buttonTrueTips": "buttonFalseTips"} onClick={this.handleClickThird}><FontAwesomeIcon icon= {faCircle} /></AnchorLink>
-                <AnchorLink href='#utilities' className={this.state.fourthButton ? "buttonTrueTips": "buttonFalseTips"} onClick={this.handleClickFourth}><FontAwesomeIcon icon= {faCircle} /></AnchorLink>
+                <Link 
+                    className="dot"
+                    activeClass="active"
+                    to='transport'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    <FontAwesomeIcon icon= {faCircle} />
+                </Link>
+                
+                <Link 
+                    className="dot"
+                    activeClass="active"
+                    to='food'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    <FontAwesomeIcon icon= {faCircle} />
+                </Link> 
+
+                <Link 
+                    className="dot"
+                    activeClass="active"
+                    to='buying'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    <FontAwesomeIcon icon= {faCircle} />
+                </Link>
+
+                <Link 
+                    className="dot"
+                    activeClass="active"
+                    to='utilities'
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    <FontAwesomeIcon icon= {faCircle} />
+                </Link> 
             </div>
         )
     }
