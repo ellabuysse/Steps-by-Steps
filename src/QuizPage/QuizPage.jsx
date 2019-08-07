033d7bbf5d52d0f1ac5c5bf514794bd7a2c660a0
Zhang <czhang.ocean@gmail.com>
2019-08-07 10:26:14 -0700

    clean up code

--- src/QuizPage/QuizPage.jsx
+++ src/QuizPage/QuizPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
-import QuizForm from '../QuizForm/QuizForm';
 import './QuizPage.css';
 import Login from '../Login/Login';
 
