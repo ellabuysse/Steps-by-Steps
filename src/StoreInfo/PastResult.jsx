521035b3b72c7ae9f5343f9fb907bcae37813545
Zhang <czhang.ocean@gmail.com>
2019-08-08 14:22:46 -0700

    formatted past quiz buttons

--- src/StoreInfo/PastResult.jsx
+++ src/StoreInfo/PastResult.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './PastResult.css';
 
 export default class PastResult extends Component {
     render() {
         return (
         <div className="result-block" onClick={() => {this.props.changeQuiz(this.props.id)}}>
                 <div></div>
-                <div>{this.props.totalPoints}</div>
+                <div className="result-number">{this.props.totalPoints}</div>
             </div>
         )
     }
